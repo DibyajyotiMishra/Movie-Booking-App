909a949b05532de4c8df53289ae2b72e771f0f56
Dibyajyoti Mishra <dibyajyotimishra14@gmail.com>
2023-06-15 23:51:32 +0530

    feat: add swagger for api documentation

--- api/App.ts
+++ api/App.ts
@@ -2,9 +2,11 @@ import cors from "cors";
 import express, { Application, NextFunction, Request, Response } from "express";
 import helmet from "helmet";
 import { Server, createServer } from "http";
+import swaggerUI from "swagger-ui-express";
 import Crypto from "./lib/crypto";
 import registerRoutes from "./routes";
 import parseResponse from "./utils/parseResponse";
+import swaggerDocument from "./swagger.json";
 
 class App {
   public express: Application;
@@ -15,6 +17,9 @@ class App {
     this.httpServer = createServer(this.express);
     this.setupMiddleWares();
     this.registerApiRoutes();
+    if (environment.isDevEnvironment() || environment.isTestEnvironment()) {
+      this.setUpDocs();
+    }
   }
 
   private setupMiddleWares(): void {
@@ -73,6 +78,19 @@ class App {
     }
 
     next();
+  }
+
+  private setUpDocs(): void {
+    const swaggerOptions: swaggerUI.SwaggerOptions = {
+      customCss: ".swagger-ui .topbar { display: none }",
+      customSiteTitle: "Movie Booking Application",
+    };
+
+    this.express.use(
+      "/docs",
+      swaggerUI.serve,
+      swaggerUI.setup(swaggerDocument, swaggerOptions)
+    );
   }
 }
 
